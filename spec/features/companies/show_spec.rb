b64cd205098e60cd0cafa16e5739ce3dd51310d3
Richard LaBrecque <richardlabreque@gmail.com>
2022-02-18 16:11:49 -0500

    feat: company index and show page

--- spec/features/companies/show_spec.rb
+++ spec/features/companies/show_spec.rb
@@ -1,31 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe 'the companies index' do
-  it 'has an index of companies that hire turing grads' do
-    visit '/companies'
-  end
-
-  it 'lists the name of the company on the index page' do
+RSpec.describe 'the company show page' do
+  it 'has a show page' do
     company_1 = Company.create!(company_name: "BigTeckInc",
                                 city: "here",
                                 state: "there",
                                 description: "stuff happens here",
                                 website: "www.thestuffhere.com")
-    visit '/companies'
+    visit "/companies/#{company_1.id}"
+  end
 
+  it 'lists a companies attribues on the show page' do
+    company_1 = Company.create!(company_name: "BigTeckInc",
+                                city: "here",
+                                state: "there",
+                                description: "stuff happens here",
+                                website: "www.thestuffhere.com")
+    visit "/companies/#{company_1.id}"
     expect(page).to have_content(company_1.company_name)
+    expect(page).to have_content(company_1.city)
     expect(page).to have_content(company_1.state)
-  end
-
-  it 'has a link to the companies show page' do
-    company_1 = Company.create!(company_name: "BigTeckInc",
-                                city: "here",
-                                state: "there",
-                                description: "stuff happens here",
-                                website: "www.thestuffhere.com")
-    visit '/companies'
-    click_on "Learn more about #{company_1.company_name}"
-    expect(current_path).to eq("/companies/#{company_1.id}")
-
+    expect(page).to have_content(company_1.description)
+    expect(page).to have_content(company_1.website)
+    expect(page).to have_content(company_1.industry)
   end
 end
